0a59d85257098f2855477ca58b8f4cc7c145cf9a
Vahnika Peddi <vahnikap@gmail.com>
2026-04-03 19:20:14 +0530

    added numpy basics

---
+++ numpy_basics.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+marks = np.array([85, 78, 90, 70, 88])
+
+print("Marks:", marks)
+print("Total:", np.sum(marks))
+print("Average:", np.mean(marks))
+print("Highest:", np.max(marks))
+print("Lowest:", np.min(marks))
+
+input("Press Enter to exit...")
